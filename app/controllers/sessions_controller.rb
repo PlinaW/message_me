74e20d3de4a29f4096a045f652cd63f62fa81ba1
PlinaW <plinski.wojciech@gmail.com>
2023-03-01 18:40:45 +0100

    Add note to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,11 @@ class SessionsController < ApplicationController
       flash[:success] = "You have successfully logged in"
       redirect_to root_path      
     else
-      flash.now[:error] = "There was something wrong with your login details"
-      render 'new'
+      flash.now[:error] = "There was something wrong with your login details" #regular fflash has to turnaround of one request response cycle
+      render 'new'                                                            # And since I'm rendering the new form again, then on the next view, 
+                                                                              # it would show up with this message and we don't want that.
+                                                                              # We wanted to show up on the same one.
+                                                                              # So we'll do render new here and use Flash now instead of just regular flash.
     end
   end
   
